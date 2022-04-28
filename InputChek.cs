3dd01eee5d5da6b8d06ddb42931a7540a8a967fd
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2022-04-28 16:48:29 +0530

    Space Check

---
+++ InputChek.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputChek : MonoBehaviour
+{
+   [SerializeField] public int point;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            point += 10;
+
+
+        }
+    }
+}
